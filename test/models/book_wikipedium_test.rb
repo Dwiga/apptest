3491cbdf20b9efbd6f0d5a48b40ed816f22da7b1
Dwi Ahmad <dwi.hyuga@gmail.com>
2022-12-03 05:31:18 +0700

    added test files

---
+++ test/models/book_wikipedium_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class BookWikipediumTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/good_read_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class GoodReadTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
