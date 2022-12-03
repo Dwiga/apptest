e131af2c43c7cbf44e7a93a7446bd95431567575
Dwi Ahmad <dwi.hyuga@gmail.com>
2022-12-03 10:09:28 +0700

    added individual detail of a book (#2)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,8 @@ class BooksController < ApplicationController
     def index
         @pagy, @records = pagy(Book.includes(:author))
     end
+
+    def view
+        @book = Book.includes(:images, :similar_books, :book_tags, :author).find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "books#index"
   get "author/:id", :to => "authors#view"
+  get "book/:id", :to => "books#view"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
