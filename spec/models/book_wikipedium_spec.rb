635ad568c60770e32bc0c99025f527224a1f813c
Dwi Ahmad <dwi.hyuga@gmail.com>
2022-12-08 11:16:46 +0700

    using let

--- spec/models/book_wikipedium_spec.rb
+++ spec/models/book_wikipedium_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe BookWikipedium, type: :model do
   context "Store data to BookWikipedium" do
-    it "found shoul not empty" do
-      author = create(:author)
-      book = create(:book, author: author)
+    let!(:author) { create(:author) }
+    let!(:book) { create(:book, author: author) }
+    let!(:book_wikipedium) { create(:book_wikipedium, book: book) }
+    it "found should not empty" do
       book_wikipedium = BookWikipedium.new(url: "https://en.wikipedia.org/wiki/Anthem_(novella)", found: "", year: -1, book_id: book.id)
       expect(book_wikipedium).to_not be_valid
       book_wikipedium.found = true
